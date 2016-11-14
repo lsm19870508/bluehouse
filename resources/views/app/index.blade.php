5b98d0e41c9ae5b998418f341f0fa1f2eaec69d8
Administrator <11>
2016-11-14 08:07:55 +0000

    composer加入redis依赖库

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -14,8 +14,8 @@
     <meta name="status" content="{{$config['status']}}"/>
     <link rel="icon" href="/client/assets/img/qm.ico">
     <title>全民联赛</title>
-    <link rel="stylesheet" href="/client/assets/css/no1basketball-07fb7d11bb.min.css">
-    <script data-main="/client/assets/js/no1basketball-3cfd7ace54.min" src="/client/assets/js/libs/r/require.js"></script>
+    <link rel="stylesheet" href="/assets/css/no1basketball-07fb7d11bb.min.css">
+    <script data-main="/assets/js/no1basketball-3cfd7ace54.min" src="/assets/js/libs/r/require.js"></script>
 </head>
 <body>
 <div class="statusbar-overlay"></div>
@@ -65,7 +65,7 @@
             <!-- page-content has additional login-screen content -->
             <div class="page-content login-screen-content auth">
                 <div class="auth-logo">
-                    <img src="/client/assets/img/auth/logo.png">
+                    <img src="/assets/img/auth/logo.png">
                 </div>
                 <!-- GetCode form -->
                 <form method="post" id="getCodeForm" action="/qmstat/code" class="form ajax-submit" enctype="application/x-www-form-urlencoded">
@@ -110,7 +110,7 @@
                         </a>
                     </div>
                     <div class="auth-logo">
-                        <img src="/client/assets/img/auth/logo.png">
+                        <img src="/assets/img/auth/logo.png">
                     </div>
                 </div>
                 <!-- Login form -->
